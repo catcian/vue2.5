0874c832578a4778a6f1f2d899acc48d811be0d6
wangchen <wangchn86@163.com>
2021-03-08 10:14:31 +0800

    7-4 图片区域 样式很有内容

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -20,31 +20,32 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl';
   .icons
-    overflow:hidden
     width: 100%
+    overflow:hidden
     height: 0
-    padding-bottom: 50%
+    padding-bottom: 50% // 宽度的 50%
     // background: green
     .icon
-      position relative
+      position relative // 针对子级 icon-img
       overflow: hidden
       float: left
       width: 25%
-      padding-bottom: 25%
+      padding-bottom: 25% // 针对宽度 25%
       // background: red
-      height:0
+      height:0 // 针对子元素 icon-img 设置 后返现宽高不相等
       .icon-img
-        position absolute
+        position absolute // 父级没有relative 相对于 html 根元素 absolute
         top:0
         left:0
         right:0
         bottom:.44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display:block
-          margin: 0 auto
-          height: 100%
+        box-sizing: border-box // 针对图片间隙
+        padding: .1rem // 针对图片间隙
+        // background: blue
+        .icon-img-content // 图标太大
+          display:block// 图片剧中 -> 块级元素
+          margin: 0 auto// 图片剧中
+          height: 100% // 针对父元素 icon-img 高度
       .icon-desc
         position absolute
         left:0
@@ -52,6 +53,6 @@ export default {
         bottom:0
         height: .44rem
         line-height: .44rem
-        text-align: center
+        text-align: center// 文字左右剧中
         color: $darkTextColor
 </style>
